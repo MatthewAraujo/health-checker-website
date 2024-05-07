6fae7d13387d1b5fbf98898f0999a331c9e5bccd
MatthewAraujo <matthewaraujo20@gmail.com>
2024-05-06 22:14:47 -0300

    fix: add request via json

--- cmd/service/check/check.go
+++ cmd/service/check/check.go
@@ -6,7 +6,9 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/MatthewAraujo/health-checker-website/types"
 	"github.com/MatthewAraujo/health-checker-website/utils"
+	"github.com/go-playground/validator"
 )
 
 type Handler struct{}
@@ -21,12 +23,25 @@ func (h *Handler) RegisterRoutes(router *http.ServeMux) *http.ServeMux {
 }
 
 func checkHandler(w http.ResponseWriter, r *http.Request) {
-	d := r.FormValue("domain")
-	if d == "" {
+	var payload types.Payload
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("validation error: %s", errors))
+		return
+	}
+
+	if payload.Domain == "" {
 		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("domain is required"))
 		return
 	}
 
+	d := payload.Domain
 	if err := Check(d); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err)
 		return
---
+++ types/types.go
@@ -0,0 +1,5 @@
+package types
+
+type Payload struct {
+	Domain string `json:"domain"`
+}
